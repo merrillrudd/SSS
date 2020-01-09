ae77f6909ec5f59ca08a4f3a2a31aa481fdff34b
merrillrudd <merrillrudd@gmail.com>
2020-01-09 15:58:26 -0800

    add argument OStype to match function SSS, with separate commands to run on Windows or OSX_Linux. Not yet tested on OSX.

--- R/Run_SS.R
+++ R/Run_SS.R
@@ -1,13 +1,20 @@
 ##' Run SS function
-##' @param path location of the executable
-##' @param ss.exe executable name for ss (e.g., "ss")
-##' @param ss.cmd command line input used when running SS
-##' @author Jason Cope
-##' @export
+#' @param path location of the executable
+#' @param ss.exe executable name for ss (e.g., "ss")
+#' @param ss.cmd command line input used when running SS
+#' @param OStype default = "Windows", option "OSX_Linux" to match function SSS (not yet tested on OSX)
+#' @author Jason Cope
+#' @export
 
-RUN.SS<-function(path, ss.exe="ss",ss.cmd=" -nohess -nox")
+RUN.SS<-function(path, ss.exe="ss",ss.cmd=" -nohess -nox", OStype = "Windows")
 {
   navigate <- paste("cd ",path,sep="")
-  command <- paste(navigate," & ",ss.exe,ss.cmd,sep="")
-  shell(command,invisible=TRUE,translate=TRUE)
+  if(OStype == "Windows"){
+  	command <- paste(navigate," & ",ss.exe,ss.cmd,sep="")
+  	shell(command,invisible=TRUE,translate=TRUE)
+  }
+  if(OStype == "OSX_Linux"){
+  	command <- paste(navigate," && chmod u+x ", ss.exe, " && ./",ss.exe,ss.cmd,sep="")
+  	system(command, invisible=TRUE)
+  }
 }
